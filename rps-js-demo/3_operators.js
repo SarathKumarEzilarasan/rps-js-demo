c68d4794273f045651edae055bfc22646de08b7e
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2025-04-21 11:25:32 +0530

    Add files via upload

---
+++ rps-js-demo/3_operators.js
@@ -0,0 +1,26 @@
+let x = 4;
+let y = 5;
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x == y);
+// console.log(x === y); // data + data types
+
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x <= y);
+// console.log(x >= y);
+
+// console.log(x >> 1);
+// console.log(x << 1);
+
+// x++;
+// x--;
+
+// x += 10;
+
+
+console.log(++x);
---
+++ rps-js-demo/6_functions.js
@@ -0,0 +1,23 @@
+// function display(){
+//     console.log("hi");
+// }
+
+// display();
+
+function add(i,j = 20){
+    return i+j;
+}
+
+// console.log(add(10,20));
+// console.log(add(10));
+// console.log(add);
+
+// let display = add;
+
+// console.log(display());
+
+let display = function (i,j = 20){
+    return i+j;
+}
+
+console.log(display(10,20));
